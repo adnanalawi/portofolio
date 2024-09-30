9faea4e90aedfcf31658bf32f9526c31bf4d4ebb
Adnnn <69080682+adnanalawi@users.noreply.github.com>
2024-09-30 22:07:25 +0700

    Update sidebar.py

--- streamlit/sidebar.py
+++ streamlit/sidebar.py
@@ -2,13 +2,9 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 
+
 def sidebar():
     st.set_page_config(layout="wide")
-
-    # Detect theme and dynamically apply styles
-    is_dark_mode = st.get_option("theme.base") == "dark"
-    text_color = "white" if is_dark_mode else "black"
-    
     with st.sidebar:
         select = option_menu(
             menu_title="",  # Judul menu
@@ -18,13 +14,7 @@ def sidebar():
             styles={
                 "container": {"padding": "5px", "background-color": "#f0f2f6"},  # Warna background sidebar
                 "icon": {"color": "blue", "font-size": "20px"},  # Warna dan ukuran ikon
-                "nav-link": { 
-                    "font-size": "16px", 
-                    "text-align": "left", 
-                    "margin": "0px", 
-                    "--hover-color": "#eeee",
-                    "color": text_color  # Dynamic text color based on theme
-                },  
+                "nav-link": { "font-size": "16px", "text-align": "left", "margin": "0px", "--hover-color": "#eeee"},  # Warna hover
                 "nav-link-selected": {"background-color": "#88cdf6", "color": "white"},  # Warna saat opsi dipilih
             }
         )
